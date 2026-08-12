4e9056db1d6a799366d99df1d5795891d5818179
Fariq <fariqn.diaz@ifabula.com>
2026-08-12 13:11:00 +0700

    Update index web config
    fix: index web config

--- index.web.js
+++ index.web.js
@@ -1,15 +1,12 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
-import appConfig from './app.json';
 
-const appName = appConfig?.name || 'Portfolio';
-
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('Portfolio', () => App);
 
 const rootTag = document.getElementById('app-root') || document.getElementById('root');
 
 if (rootTag) {
-  AppRegistry.runApplication(appName, {
+  AppRegistry.runApplication('Portfolio', {
     initialProps: {},
     rootTag: rootTag,
   });
